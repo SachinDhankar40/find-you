36b8059edcc3629847e1c07802a524be4d8dbbde
sdpyhackit <sachin@goognu.com>
2020-05-24 07:05:18 +0530

    user views changed

--- findyouproject/Content/api/urls.py
+++ findyouproject/Content/api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('/content-user', views.ContentUserCreate.as_view(),name="ContentUserCreate"),
     path('/content-user-update', views.ContentUserUpdate.as_view(),name="ContentUserUpdate"),
     path('/content-user-update-pic', views.ContentUserUpdatePic.as_view(),name="ContentUserUpdatePic"),
+    path('/content-listing', views.ContentContentList.as_view(),name="ContentContentList"),
 ]
